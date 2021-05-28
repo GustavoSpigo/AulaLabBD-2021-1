a2b3c33e4f26e06fbc723478874dc00ba2dd5fc4
Gustavo Gomes <gustavo@spigo.net>
2021-05-27 21:09:46 -0300

    Aula 2021-05-21

--- ator.php
+++ ator.php
@@ -10,10 +10,11 @@
 ?>
 <div class="mdl-grid">
     <div class="mdl-cell mdl-cell--12-col">
-        <iframe src="" id="ifr" name="ifr" style="display: none"></iframe>
-        <button id="show-dialog" type="button"
-            class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">Novo
-            registro</button>
+        <iframe src="" id="ifr" name="ifr" style="/*display: none;*/ border: solid black"></iframe>
+        <button id="show-dialog" type="button" onclick="mostrarDialogoInserir()"
+            class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+            Novo registro
+        </button>
         <br><br>
         <table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
             <thead>
@@ -45,12 +46,11 @@
                             <i class="material-icons">edit</i>
                         </button>
 
-                        <form onsubmit="return window.confirm('Tem certeza?');" action="ator-delete.php" method="post" target="ifr" style="display:inline">
-                            <input type="hidden" name="ator_id" value="<?php echo $linha['ator_id']; ?>">
-                            <button type="submit" class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored">
-                                <i class="material-icons">delete_forever</i>
-                            </button>
-                        </form>
+                        
+                        <button class="mdl-button mdl-js-button mdl-button--icon mdl-button--colored"
+                                onclick="perguntarDelete(<?php echo $linha['ator_id']; ?>)">
+                            <i class="material-icons">delete_forever</i>
+                        </button>
                     </td>
                 </tr>
                 <?php } ?>
@@ -58,78 +58,69 @@
         </table>
     </div>
 </div>
-<dialog class="mdl-dialog" id="dialog-insert" style="width: fit-content;">
-    <form action="ator-inserir.php" method="post" target="ifr">
-        <h4 class="mdl-dialog__title">Inserir novo item</h4>
-        <div class="mdl-dialog__content">
-            <p>
-            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                <input class="mdl-textfield__input" type="text" name="primeiro_nome">
-                <label class="mdl-textfield__label" >Primeiro nome</label>
-            </div>
-            <br>
-            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                <input class="mdl-textfield__input" type="text" name="ultimo_nome">
-                <label class="mdl-textfield__label" >Último nome</label>
-            </div>
-            </p>
-        </div>
-        <div class="mdl-dialog__actions">
-            <button type="submit" class="mdl-button submit">Inserir</button>
-            <button type="button" class="mdl-button close">Fechar</button>
-        </div>
-    </form>
-</dialog>
-<script>
-    //Dialogo de inserir
-    var dialog = document.querySelector('#dialog-insert');
-    var showDialogButton = document.querySelector('#show-dialog');
-    if (!dialog.showModal) {
-        dialogPolyfill.registerDialog(dialog);
-    }
-    showDialogButton.addEventListener('click', function() {
-        dialog.showModal();
-    });
-    dialog.querySelector('.close').addEventListener('click', function() {
-        dialog.close();
-    });
-</script>
-
-
-<dialog class="mdl-dialog" id="dialog-edit" style="width: fit-content;">
-    <form action="ator-alterar.php" method="post" target="ifr">
-        <h4 class="mdl-dialog__title">Inserir novo item</h4>
+<dialog class="mdl-dialog" id="dialog" style="width: fit-content;">
+    <form action="ator-acoes.php" method="post" target="ifr" id="form-insupd">
+        <input type="hidden" name="acao" id="acao">
+        <h4 class="mdl-dialog__title">Dados do ator</h4>
         <div class="mdl-dialog__content">
             <p>
             <input type="hidden" name="ator_id" id="ator_id">
             <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                <input class="mdl-textfield__input" type="text" id="primeiro_nome" name="primeiro_nome">
+                <input class="mdl-textfield__input" type="text" id="primeiro_nome" name="primeiro_nome" onkeyup="this.setCustomValidity('');this.parentElement.MaterialTextfield.checkValidity()">
                 <label class="mdl-textfield__label" for="primeiro_nome">Primeiro nome</label>
             </div>
             <br>
             <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                <input class="mdl-textfield__input" type="text" id="ultimo_nome" name="ultimo_nome">
+                <input class="mdl-textfield__input" type="text" id="ultimo_nome" name="ultimo_nome" 
+                       onkeyup="this.setCustomValidity('');this.parentElement.MaterialTextfield.checkValidity()">
                 <label class="mdl-textfield__label" for="ultimo_nome">Último nome</label>
             </div>
             </p>
         </div>
         <div class="mdl-dialog__actions">
-            <button type="submit" class="mdl-button submit">Alterar</button>
-            <button type="button" class="mdl-button" onclick="document.getElementById('dialog-edit').close()">Fechar</button>
+            <button type="submit" class="mdl-button submit">Salvar</button>
+            <button type="button" class="mdl-button" onclick="document.getElementById('dialog').close()">Fechar</button>
         </div>
     </form>
 </dialog>
+<form action="ator-acoes.php" method="post" target="ifr" id="form-delete" >
+    <input type="hidden" name="acao" value="delete">
+    <input type="hidden" name="ator_id" id="ator_id_delete" value="">
+</form>
 <script>
     //Dialogo de Editar
     function mostrarDialogEdicao(v_ator_id, v_primeiro_nome , v_ultimo_nome){
-
+        document.getElementById('acao'          ).value = "alterar";
         document.getElementById('ator_id'       ).value = v_ator_id;
         
         document.getElementById('primeiro_nome' ).parentElement.MaterialTextfield.change(v_primeiro_nome);
         document.getElementById('ultimo_nome'   ).parentElement.MaterialTextfield.change(v_ultimo_nome);
 
-        document.getElementById('dialog-edit').showModal();
+        document.getElementById('dialog').showModal();
     }
+
+    function mostrarDialogoInserir(){
+        document.getElementById('acao').value = "inserir";
+        document.getElementById('form-insupd').reset();
+
+        document.getElementById('dialog').showModal();
+    }
+
+    var confirmarDelete = function(){
+        document.getElementById('aviso-toast').classList.remove("mdl-snackbar--active");
+        document.getElementById('form-delete').submit();
+    }
+    function perguntarDelete(v_ator_id){
+        document.getElementById('ator_id_delete').value = v_ator_id;
+        var data = {
+            message: 'Tem certeza?',
+            timeout: 5000,
+            actionHandler: confirmarDelete,
+            actionText: 'Sim'
+        };
+        document.getElementById('aviso-toast').MaterialSnackbar.showSnackbar(data);
+    }
+
 </script>
 
 <div id="aviso-toast" class="mdl-js-snackbar mdl-snackbar">
